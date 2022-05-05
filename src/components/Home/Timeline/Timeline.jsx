c79bf4b70b8a684371409c218def6c4fb3084fed
Cecilia Torres <cecilia.torres@htech.com>
2022-05-04 17:45:35 -0700

    Timeline progress

---
+++ src/components/Home/Timeline/Timeline.jsx
@@ -0,0 +1,13 @@
+import Post from './Post';
+
+const Timeline = () => {
+  return (
+    <div className='bg-gray-100 w-full pt-4'>
+      { new Array(10).fill(0).map((_, index) => (
+        <Post key={index} postData={{ avatarURL: 'images/avatars/mariela.jpeg', altText: 'mariela', username: 'Mariela' }} />
+      )) }
+      </div>
+  );
+}
+ 
+export default Timeline;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Home/Header/Header";
 import Stories from "../components/Home/Stories/Stories";
+import Timeline from "../components/Home/Timeline/Timeline";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Stories />
+      <Timeline />
     </>
   );
 };
